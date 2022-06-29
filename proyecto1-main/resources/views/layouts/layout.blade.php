5a0721bbae965c0776f08b6c334559447c5a523a
Sebastian Contador <seba_bakan123@hotmail.com>
2022-06-29 04:49:35 -0400

    se elimino el box shadow

--- proyecto1-main/resources/views/layouts/layout.blade.php
+++ proyecto1-main/resources/views/layouts/layout.blade.php
@@ -69,9 +69,5 @@
   </footer>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
-<style>
-  footer{
-    box-shadow: 0 -1px 0 rgba(0, 0, 0, 0.2);
-  }
-</style>
+
 </html>
